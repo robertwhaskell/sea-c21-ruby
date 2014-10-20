76952a5d43d2fb15a6d44431b2e574fabe4c4b57
Robert <robertwhaskell@gmail.com>
2014-10-20 16:16:28 -0700

    homework 3 resubmit

--- lib/class3/exercise4.rb
+++ lib/class3/exercise4.rb
@@ -7,10 +7,8 @@
 # Years divisible by 100 are not leap years (e.g. 1900) unless they are also
 # divisible by 400 (e.g. 2000).
 #
-<<<<<<< HEAD
 # Tip: I expect you to use a loop and a few modulo operations.
 
 (1900..2000).each { |i| puts i if i % 400 == 0 || i % 4 == 0 && i % 100 != 0 }
-=======
+
 # TIP: I expect you to use a loop and a few modulo operations.
->>>>>>> 35598e4c637577494df05b39bbf51321cff468e0
